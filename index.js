764ee8908ecd1bafb1ad21f0ba6aae8cecad181e
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-07-13 12:23:49 +0300

    Adds addition function

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function divideBy()
         num2 = document.getElementById("secondNumber").value;
 document.getElementById("result").innerHTML = num1 / num2;
 }
+function addBy{
+    num1=document.getElementById('firstNumber').value;
+    num2=document.getElementById('secondNumber').value;
+}
